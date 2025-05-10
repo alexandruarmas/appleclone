6acf4ee8fa889b18db329f665a88baf82e38f3c5
Alexandru Armas <alexandruarmas02@gmail.com>
2025-05-10 13:53:19 +0300

    Fix linter errors in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
+// React and third-party libraries
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
+// Application imports
 import App from "./App.jsx";
+
+// Styles
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
